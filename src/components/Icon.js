719575c4a7ff6d50dca9d2d7ef3dfc364cab2502
khalil0525 <collinskhalil@hotmail.com>
2022-08-03 17:04:10 -0400

    created ArrowIcon component

---
+++ src/components/Icon.js
@@ -0,0 +1,15 @@
+export const ArrowIcon = (props) => {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 17 10"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.8995 14.0715L16.9706 7.00042M16.9706 7.00042L14.6135 7.00042L0 7.00043M16.9706 7.00042L10.4887 0.518613"
+        stroke="currentColor"
+      ></path>
+    </svg>
+  );
+};
